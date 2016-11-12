f4551f791a9d0e1683fac5495853930e58d8c5ab
tomkarho <tomkarho@gmail.com>
2016-11-12 12:53:02 +0200

    Initialize startDate and endDate fields and default values.

--- sprintGenerator.js
+++ sprintGenerator.js
@@ -2,10 +2,20 @@
  * Created by tomkarho on 11.11.2016.
  */
 (function ($, moment) {
+    /*date formats*/
+    var ymd = "Y-M-D";
 
+    /*properties*/
+    var startDateField;
+    var endDateField;
 
+    /*methods*/
     var initialize = function () {
+        startDateField = $("#startDate");
+        startDateField.val(moment().format(ymd));
 
+        endDateField = $("#endDate");
+        endDateField.val(moment().add(7, "days").format(ymd));
     };
 
     $(document).ready(initialize);
