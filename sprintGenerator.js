31942020cbd32e271a2d37aa753ef92f1d3018a8
tomkarho <tomkarho@gmail.com>
2016-11-12 12:48:27 +0200

    Start out sprintGenerator javascript

---
+++ sprintGenerator.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tomkarho on 11.11.2016.
+ */
+(function ($, moment) {
+
+
+    var initialize = function () {
+
+    };
+
+    $(document).ready(initialize);
+})(jQuery, moment);
